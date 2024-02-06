49122773d14f361e1c6233b002b6623c6314cc9b
Sadra <sadra.kabiri@gmail.com>
2024-02-06 14:46:08 +0330

    responsive banner and attributes

--- components/modules/Banner.js
+++ components/modules/Banner.js
@@ -3,7 +3,7 @@ import styles from "./Banner.module.css";
 export default function Banner() {
   return (
     <div className={styles.container}>
-      <div className={styles.lefts}>
+      <div className={styles.left}>
         <h1>SodoFood</h1>
         <h2>Food Delivery and Takeout!</h2>
         <p>
